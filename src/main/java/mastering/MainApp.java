7a9f1ba4131a1bcee7a81de011733d19b5ac3cd9
decisiongate <decisiongate.com@gmail.com>
2020-10-31 23:12:23 +0100

    update

--- src/main/java/mastering/MainApp.java
+++ src/main/java/mastering/MainApp.java
@@ -2,47 +2,61 @@ package mastering;
 
 import javax.swing.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class MainApp {
 
     public static void main(String[] args) {
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    StringCalculator stringCalc = new StringCalculator();
-                    stringCalc.stringCalcutatorTask();
-                } catch (Exception ex) {
-                    System.out.print("Error: "+ex.toString());
-                }
-            }
-        }).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ArraysMethods arraysMethods = new ArraysMethods();
-                    arraysMethods.Tasks();
-                } catch (Exception ex) {
-                    System.out.print("Error: "+ex.toString());
-                }
-            }
-        }).start();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select menu option: ");
+        String menuOption = scanner.nextLine();
+        System.out.println("Selected menu option: " + menuOption);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-/*     *******************************     */
-
-/*     *******************************     */
-                } catch (Exception ex) {
-                    System.out.print("Error: "+ex.toString());
+        if(menuOption.equals("1")) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        StringCalculator stringCalc = new StringCalculator();
+                        stringCalc.stringCalcutatorTask();
+                    } catch (Exception ex) {
+                        System.out.print("Error: " + ex.toString());
+                    }
                 }
-            }
-        }).start();
+            }).start();
+        }
+
+        if(menuOption.equals("2")) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        ArraysMethods arraysMethods = new ArraysMethods();
+                        arraysMethods.Tasks();
+                    } catch (Exception ex) {
+                        System.out.print("Error: " + ex.toString());
+                    }
+                }
+            }).start();
+        }
+
+        if(menuOption.equals("3")) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        /*     *******************************     */
+                        Operators operators = new Operators();
+                        operators.Tasks();
+                        /*     *******************************     */
+                    } catch (Exception ex) {
+                        System.out.print("Error: " + ex.toString());
+                    }
+                }
+            }).start();
+        }
 
     }
 }
---
+++ src/main/java/mastering/Operators.java
@@ -0,0 +1,29 @@
+package mastering;
+
+public class Operators {
+
+    public void Tasks() {
+
+        System.out.println((5 > 3) && (8 > 5));  // true
+        System.out.println((5 > 3) && (8 < 5));  // false
+        System.out.println((5 < 3) || (8 > 5));  // true
+        System.out.println((5 > 3) || (8 < 5));  // true
+        System.out.println((5 < 3) || (8 < 5));  // false
+        System.out.println(!(5 == 3));  // true
+        System.out.println(!(5 > 3));  // false
+
+        int a, b;
+        a = 10;
+        b = (a == 1) ? 20: 30;
+        System.out.println( "Value of b is : " +  b );
+        b = (a == 10) ? 20: 30;
+        System.out.println( "Value of b is : " + b );
+
+        String name = "Test";
+        // following will return true since name is type of String
+        boolean result = name instanceof String;
+        System.out.println( result );
+
+    }
+
+}
